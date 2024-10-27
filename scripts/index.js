b28e9cd8589a81cfa45086fe9937e3e3b5378106
teantesimmons <tjs1893@gmail.com>
2024-10-27 04:05:33 -0400

    add js file with array list

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+const initialCards = [
+  {name: "Studio Apartment or Apartment Studio",
+    link: "https://images.unsplash.com/photo-1511379938547-c1f69419868d?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+  {name: "City in the Sky",
+    link: "https://images.unsplash.com/photo-1729545312568-6458b2d6ffae?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+  {name: "Millinium Park",
+    link: "https://images.unsplash.com/photo-1729560385431-645049bf5821?q=80&w=2835&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+  {name: "Desert Valley",
+    link: "https://images.unsplash.com/photo-1729538660620-1691d1a6b434?q=80&w=2367&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+  {name: "A house in the middle of a park",
+    link: "https://images.unsplash.com/photo-1729731322197-0d9282935266?q=80&w=2667&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+  {name: "Tower",
+    link: "https://images.unsplash.com/photo-1729560386606-9688cc485e8c?q=80&w=2835&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+]
